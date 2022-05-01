8f332e8807d93e00fc02235970e6f728ea856b99
Álvaro <adepablos@edu.tecnocampus.cat>
2022-05-01 14:28:57 +0200

    Create dummy

---
+++ Assets/Scripts/HealthSystem/EnemyHealthSystem.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyHealthSystem : MonoBehaviour, ICanTakeDmg
+{
+    private static float maxHp = 100f;
+    private float hp = 100f;
+
+    public float getCurrentHealth()
+    {
+        return hp;
+    }
+
+    public float getMaxHealth()
+    {
+        return maxHp;
+    }
+
+    public void TakeDmg(float dmg)
+    {
+        hp -= dmg;
+    }
+}
